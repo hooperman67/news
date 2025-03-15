e76141f7d45e9cf62dbc940b965f150fb7e7f3e5
hooperman67 <84323120+hooperman67@users.noreply.github.com>
2025-03-15 17:02:46 +0000

    Update parse.php

--- parse.php
+++ parse.php
@@ -10,6 +10,15 @@ $feed->init();
 $feed->handle_content_type();
  
 // Let's begin our XHTML webpage code.  The DOCTYPE is supposed to be the very first thing, so we'll keep it on the same line as the closing-PHP tag.
+		$html .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ 
+<html xmlns
+<head>
+	<title>Sample SimplePie Page</title>
+	<meta
+</head>
+<body><div class="item">';
+
 ?>
  
 	<div class="header">
@@ -23,15 +32,6 @@ $feed->handle_content_type();
 	*/
 	foreach ($feed->get_items() as $item):
 
- 
-		$html .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
- 
-<html xmlns
-<head>
-	<title>Sample SimplePie Page</title>
-	<meta
-</head>
-<body><div class="item">';
 			$html .= '<h3><a rel="nofollow" target="_blank" href="' . $item->get_permalink() . '">' . $item->get_title() . '</a></h3>';
 			$html .= '<p>'. $item->get_date() .'</p><p>'. $item->get_description() . '</p>';
 		$html .= '</div></body>
